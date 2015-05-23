a72cd9cc62a54421b745fcd70fb513d8e4929f4f
Edwin Lunandy <populationtwo@gmail.com>
2015-05-22 21:43:40 -0400

    Add angular ui router.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,30 +15,43 @@ var app = angular
 		'ngAnimate',
 		'ngCookies',
 		'ngResource',
-		'ngRoute',
+		//'ngRoute',
 		'ngSanitize',
 		'ngTouch',
 		'edmundsLibrary',
-		'ui.bootstrap'
+		'ui.bootstrap',
+		'ui.router'
 	] )
-	.config( function ($routeProvider) {
-		$routeProvider
-			.when( '/', {
-				templateUrl: 'views/main.html',
-				controller : 'MainCtrl'
+	.config( function ($stateProvider, $urlRouterProvider) {
+		//
+		// For any unmatched url, redirect to /state1
+		$urlRouterProvider.otherwise( '/' );
+		//
+		// Now set up the states
+		$stateProvider
+			.state( 'home', {
+				url        : '/',
+				templateUrl: 'views/main.html'
 			} )
-			.when( '/about', {
+			.state( 'about', {
+				url        : '/about',
 				templateUrl: 'views/about.html',
 				controller : 'AboutCtrl'
 			} )
-			.when( '/compare', {
-				templateUrl: 'views/compare.html'
+			.state( 'compare-landing', {
+				url        : '/compare',
+				//templateUrl: 'views/compare.html',
+				views: {
+					'pageheader': {
+						templateUrl: 'views/partials/header.html'
+					},
+					'content': {
+						templateUrl: 'views/compare.html'
+					}
+				}
 			} )
-			.when( '/compare/model', {
+			.state( 'compare', {
+				url        : '/compare/model',
 				templateUrl: 'views/compare-model.html'
-			} )
-			.otherwise( {
-				redirectTo: '/'
 			} );
 	} );
-
