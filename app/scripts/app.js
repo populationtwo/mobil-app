f86ea0d8102e8cc5fe5d9544556e78041a027537
Edwin Lunandy <populationtwo@gmail.com>
2015-05-26 20:50:35 -0400

    Refactor views.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,54 +11,40 @@
  * Main module of the application.
  */
 var app = angular
-	.module( 'mobilApp', [
-		'ngAnimate',
-		'ngCookies',
-		'ngResource',
-		//'ngRoute',
-		'ngSanitize',
-		'ngTouch',
-		'edmundsLibrary',
-		'ui.bootstrap',
-		'ui.router'
-	] )
-	.config( function ($stateProvider, $urlRouterProvider) {
-		//
-		// For any unmatched url, redirect to /state1
-		$urlRouterProvider.otherwise( '/' );
-		//
-		// Now set up the states
-		$stateProvider
-			.state( 'home', {
-				url        : '/',
-				templateUrl: 'views/main.html'
-			} )
-			.state( 'about', {
-				url        : '/about',
-				templateUrl: 'views/about.html',
-				controller : 'AboutCtrl'
-			} )
-			.state( 'compare-landing', {
-				url        : '/compare',
-				//templateUrl: 'views/compare.html',
-				views: {
-					'pageheader': {
-						templateUrl: 'views/partials/header.html'
-					},
-					'content': {
-						templateUrl: 'views/compare.html'
-					}
-				}
-			} )
-			.state( 'compare', {
-				url        : '/compare/model',
-				views: {
-					'pageheader': {
-						templateUrl: 'views/partials/header.html'
-					},
-					'content': {
-						templateUrl: 'views/compare-model.html'
-					}
-				}
-			} );
-	} );
+  .module( 'mobilApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    //'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'edmundsLibrary',
+    'ui.bootstrap',
+    'ui.router'
+  ] )
+  .config( function ($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise( '/' );
+    //
+    // Now set up the states
+    $stateProvider
+
+
+      .state( 'home', {
+        url  : '/',
+        templateUrl: 'views/main.html'
+
+      } )
+      .state( 'compare', {
+        url  : '/compare',
+        templateUrl: 'views/compare-result.html'
+
+
+      } )
+      .state( 'about', {
+        url        : '/about',
+        templateUrl: 'views/about.html',
+        controller : 'AboutCtrl'
+      } );
+  } );
